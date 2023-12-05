2a6d84f19feb7fdffdc2113d8b4f55d2d05a37bd
Jailton Mocitaiba <102435314+jailtoncmf@users.noreply.github.com>
2023-12-05 07:04:06 -0300

    Update produtos.sql

--- produtos.sql
+++ produtos.sql
@@ -8,15 +8,10 @@ CREATE TABLE produtos (
 
 INSERT INTO produtos (id, marca, produto, estoque, preco) 
 VALUES 
-(1, 'Ruffles', 'Batata Chips', 9, 2.5),
-(2, 'Doritos', 'Salgadinho Nacho', 5, 5);
-(3, 'Amendoim', 'Amendoim', 2, 5);
-
+(x, 'x', 'x', y, x.y);
 
 SELECT * FROM produtos;
 
-UPDATE produtos SET estoque = 8 WHERE id = 1;
-UPDATE produtos SET estoque = 1 WHERE id = 3;
-UPDATE produtos SET estoque = 4 WHERE id = 2;
+UPDATE produtos SET preco = x WHERE id = x;
 
-DELETE FROM produtos;
+DELETE FROM produtos WHERE id = x;
